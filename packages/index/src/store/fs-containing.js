5771cb91a36358a23221583bda14e0fa211b7da5
Vasco Santos <santos.vasco10@gmail.com>
2025-03-25 13:16:52 +0100

    fix: rename fs blob store and fix make blob store handle undefined on encode

--- packages/index/src/store/fs-containing.js
+++ packages/index/src/store/fs-containing.js
@@ -10,6 +10,7 @@ import {
   encode as indexRecordEncode,
   decode as indexRecordDecode,
 } from '../record.js'
+import { removeUndefinedRecursively } from './utils.js'
 
 /**
  * File System implementation of ContainingIndexStore
@@ -150,30 +151,3 @@ export class FSContainingIndexStore {
 }
 
 export const type = 'index/containing@0.1'
-
-/**
- * @param {any} input
- * @returns {any}
- */
-function removeUndefinedRecursively(input) {
-  if (Array.isArray(input)) {
-    // Process arrays recursively but do NOT touch non-undefined values like Uint8Array
-    return input.map(removeUndefinedRecursively).filter((v) => v !== undefined)
-  }
-
-  if (
-    input &&
-    typeof input === 'object' &&
-    Object.getPrototypeOf(input) === Object.prototype
-  ) {
-    // Only process plain objects, ignoring things like Uint8Array
-    return Object.fromEntries(
-      Object.entries(input)
-        .map(([key, value]) => [key, removeUndefinedRecursively(value)])
-        .filter(([_, value]) => value !== undefined)
-    )
-  }
-
-  // Return all other types as they are
-  return input
-}
