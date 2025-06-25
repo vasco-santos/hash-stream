68d3958c541874b0224ee84cc48fbc1ee400c017
Vasco Santos <santos.vasco10@gmail.com>
2025-06-25 12:38:47 +0200

    fix: pack exported modules (#159)

--- packages/streamer/test/index.node.test.js
+++ packages/streamer/test/index.node.test.js
@@ -1,6 +1,12 @@
+import * as API from '@hash-stream/pack/types'
+
 import fs from 'fs'
 import os from 'os'
 import path from 'path'
+
+// HTTP Store
+import { HTTPPackStore } from '@hash-stream/pack/store/http'
+import { MemoryIndexStore } from '@hash-stream/index/store/memory'
 
 // FS Stores
 import { FSIndexStore } from '@hash-stream/index/store/fs'
@@ -13,6 +19,7 @@ import { S3LikeIndexStore } from '@hash-stream/index/store/s3-like'
 import { runHashStreamTests } from './hash-streamer.js'
 
 import { createS3Like, createBucket } from './helpers/resources.js'
+import { createInMemoryHTTPServer } from './helpers/http-server.js'
 ;[
   {
     name: 'FS',
@@ -86,6 +93,49 @@ import { createS3Like, createBucket } from './helpers/resources.js'
         destroy: () => {},
       })
       return destroyablePackStore
+    },
+  },
+  {
+    name: 'HTTP',
+    /**
+     * @returns {Promise<import('./hash-streamer.js').DestroyablePackStore>}
+     */
+    getPackStore: async () => {
+      const httpServer = createInMemoryHTTPServer()
+      const { baseURL, store } = await httpServer.start()
+
+      const packStore = new HTTPPackStore({
+        url: baseURL,
+      })
+      const destroyablePackStore = Object.assign(packStore, {
+        directory: '',
+        /**
+         * Put a pack file in S3.
+         *
+         * @param {API.MultihashDigest | API.Path} target
+         * @param {Uint8Array} data - The pack file bytes.
+         */
+        put: async (target, data) => {
+          const key = packStore._getObjectKey(target)
+          store.set(`/${key}`, data)
+        },
+        destroy: () => {
+          return httpServer.stop()
+        },
+      })
+      return Promise.resolve(destroyablePackStore)
+    },
+    /**
+     * @returns {Promise<import('./hash-streamer.js').DestroyableIndexStore>}
+     */
+    getIndexStore: () => {
+      const indexStore = new MemoryIndexStore()
+      const destroyableIndexStore = Object.assign(indexStore, {
+        direntory: '',
+        destroy: () => {},
+      })
+      // @ts-expect-error
+      return Promise.resolve(destroyableIndexStore)
     },
   },
 ].forEach(({ name, getIndexStore, getPackStore }) => {
